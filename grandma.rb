2c21e25d53499ce2cb988c4bb29f5b12b455dfcc
Iskra B <ibatistapoblete@gmail.com>
2019-03-20 15:33:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,6 @@
-def speak_to_grandma(phrase)
-  if phrase != phrase.upcase()
-    return "HUH?! SPEAK UP, SONNY!"
-    elsif grandma == "I LOVE YOU GRANDMA!".upcase
-      return "I LOVE YOU TOO PUMPKIN!"
-    else
-      return "NO, NOT SINCE 1938!"
-      end
-      end
+if grandma == "shout".upcase
+  puts "NO, NOT SINCE 1938!"
+elsif grandma == "I LOVE YOU GRANDMA!".upcase
+  puts "I LOVE YOU TOO PUMPKIN!"
+else grandma == "say".downcase
+  puts "HUH?! SPEAK UP, SONNY".upcase
